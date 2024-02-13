881789af2f74b5e5951ffe31ed4fe3c79ab00a57
pawelce <szkolacecot@gmail.com>
2024-02-13 10:37:01 +0100

    alalalalallala

---
+++ 2CT/Kalkulator/Kalkulator.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+class Dodawanie {
+		public:
+			int liczba1;
+			int	liczba2;
+			int oblicz() {
+				return this->liczba1 + this->liczba2;
+			}
+};		
+class Odejmowanie {
+		public:
+			int liczba1;
+			int	liczba2;
+			int oblicz() {
+				return this->liczba1 - this->liczba2;
+			}
+};
+class Mnozenie {
+		public:
+			int liczba1;
+			int	liczba2;
+			int oblicz() {
+				return this->liczba1 * this->liczba2;
+			}
+};
+
+int main()
+{
+	int a, b;
+	setlocale(LC_ALL, " ");
+	cout << "Podaj a = "; cin >> a;
+	cout << "Podaj b = "; cin >> b;
+
+	Dodawanie dodawanie;
+	dodawanie.liczba1 = a;
+	dodawanie.liczba2 = b;
+	cout << "Wynik dodawania = " << dodawanie.oblicz() << endl;
+	Odejmowanie odejmowanie;
+	odejmowanie.liczba1 = a;
+	odejmowanie.liczba2 = b;
+	cout << "Wynik odejmowania = " << odejmowanie.oblicz() << endl;
+	Mnozenie mnozenie;
+	mnozenie.liczba1 = a;
+	mnozenie.liczba2 = b;
+	cout << "Wynik mnozenia = " << mnozenie.oblicz() << endl;
+
+	return EXIT_SUCCESS;
+}
