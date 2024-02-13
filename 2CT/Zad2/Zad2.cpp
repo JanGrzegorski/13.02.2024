2c95a6ce7de4bae3d9683f7d04dac75f30ee3c40
pawelce <szkolacecot@gmail.com>
2024-02-13 10:20:07 +0100

    bambam

---
+++ 2CT/Zad2/Zad2.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+using namespace std;
+
+class Maszyna {
+public:
+	string name;
+	string color;
+	int serial_number;
+	int digit;
+
+	void runMaszyna() {
+		if (digit == 2) {
+			showMaszyna();
+		}
+		else if (digit == 1) {
+			zrobMaszyna();
+		}
+		else {
+			message();
+		}
+	}
+	void showMaszyna() {
+		cout << "Maszyna uruchomiona: nr - " << serial_number << " Nazwa: "
+			<< name << "Kolor: " << color << endl;
+	}
+	void zrobMaszyna() {
+		cout << "Maszyna uruchomiona: nr - " << serial_number << " Nazwa: "
+			<< name << "Kolor: " << color << endl;
+	}
+
+	void message() {
+		cout << "Maszyna wybuchla" << endl;
+	}
+};
+
+
+
+
+int main()
+{
+	int uruchomienie;
+	cout << "Maszyna 1 - Start, inna wartosc: Maszyna bumb bum " << endl;
+	cout << "Co chcesz zrobic: "; cin >> uruchomienie;
+	cout << "----------------------------------------" << endl;
+	int easteregg;
+	cout << "Maszyna 2 - Start, inna wartosc: Maszyna nie dziala nie ma 21:37 " << endl;
+	cout << "Co chcesz zrobic: "; cin >> easteregg;
+	cout << "----------------------------------------" << endl;
+	Maszyna mojamasz;
+	mojamasz.digit = uruchomienie;
+	mojamasz.serial_number = 123456788;
+	mojamasz.name = "Opel Astra 1 ";
+	mojamasz.color = "szybki";
+	mojamasz.runMaszyna();
+	Maszyna niemojamasz;
+	niemojamasz.digit = easteregg;
+	niemojamasz.serial_number = 2137;
+	niemojamasz.name = "Barka Jana Pawla II ";
+	niemojamasz.color = "Kremowkowy";
+	niemojamasz.runMaszyna();
+
+	return EXIT_SUCCESS;
+
+}
+
---
+++ 2CT/zad1_OOP/zad1_OOP.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+
+class Robot {
+	public:
+		string name;
+		int serial_number;
+		int digit;
+
+		void runRobot() {
+			if (digit == 1) {
+				showRobot();
+			}
+			else {
+				message();
+			}
+		}
+		void showRobot() {
+			cout << "Robot uruchomiony: nr - " << serial_number << " Nazwa: "
+				<< name << endl;
+		}
+		void message() {
+			cout << "Robot nie uruchomiony" << endl;
+		}
+};
+
+
+
+
+int main()
+{
+	int uruchomienie;
+	cout << "Robot 1 - Start, inna wartość: Robot nie działa " << endl;
+	cout << "Co chcesz zrobic: "; cin >> uruchomienie;
+	cout << "----------------------------------------" << endl;
+	Robot mojrobocik;
+	mojrobocik.digit = uruchomienie;
+	mojrobocik.serial_number = 123456788;
+	mojrobocik.name = "Opel Astra 1 ";
+	mojrobocik.runRobot();
+
+
+
+
+
+	return EXIT_SUCCESS;
+
+}
+
